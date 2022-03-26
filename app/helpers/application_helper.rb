7171ea7fa6f22c341b737ee0803f98423f7e59a1
Trevin Wilkinson <trevin.wilkinson@gmail.com>
2022-03-25 21:48:29 -0700

    Add helper to add .active class to elements

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def active_class(params_controller, current_controller)
+    if params_controller == current_controller
+      'active'
+    else
+      ''
+    end
+  end
 end
