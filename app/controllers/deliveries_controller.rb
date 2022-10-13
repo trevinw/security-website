32d2a8e98877df11dddadf9746e3a9777cc1afd8
Trevin Wilkinson <trevin.wilkinson@gmail.com>
2022-10-13 03:18:23 -0700

    Fix Dates

--- app/controllers/deliveries_controller.rb
+++ app/controllers/deliveries_controller.rb
@@ -1,22 +1,26 @@
 class DeliveriesController < ApplicationController
   before_action :set_delivery, only: %i[show edit update destroy]
   before_action :set_users, only: %i[index new edit]
+  before_action :set_datetime_time_zone, only: :create
 
   def index
-    if params[:start_date] && params[:end_date]
-      start_date = DateTime.parse(params[:start_date])
-      begin
-        end_date = DateTime.parse(params[:end_date])
-      rescue ArgumentError => _e
-        end_date = DateTime.current.in_time_zone('Pacific Time (US & Canada)').end_of_day
-      end
-      @deliveries = Delivery.where(datetime: start_date..end_date)
-      @date_string = "Deliveries from #{start_date.strftime('%m/%d/%y')} - #{end_date.strftime('%m/%d/%y')}"
-    else
-      @deliveries = Delivery.where(datetime: DateTime.now.midnight).order(datetime: :desc)
-      @date_string = "Deliveries for #{DateTime.now.strftime('%m/%d/%y')}"
-    end
+    start_date = if params[:start_date].present?
+                   params[:start_date].in_time_zone('Pacific Time (US & Canada)').midnight
+                 else
+                   DateTime.now.midnight.to_time
+                 end
+    end_date = if params[:end_date].present?
+                 params[:end_date].in_time_zone('Pacific Time (US & Canada)').end_of_day
+               else
+                 DateTime.now.end_of_day.to_time
+               end
+    @date_string = if params[:start_date]
+                     "Deliveries from #{start_date.strftime('%m/%d/%y')} - #{end_date.strftime('%m/%d/%y')}"
+                   else
+                     "Deliveries for #{DateTime.now.strftime('%m/%d/%y')}"
+                   end
 
+    @deliveries = Delivery.where(datetime: start_date..end_date).order(datetime: :desc)
     @delivery = Delivery.new
   end
 
@@ -60,6 +64,12 @@ class DeliveriesController < ApplicationController
 
   def set_users
     @users = User.active.with_any_role(:admin, :security_lead, :security_rover).sort_by(&:badge)
+  end
+
+  def set_datetime_time_zone
+    return unless params[:delivery][:datetime].present?
+
+    params[:delivery][:datetime] = params[:delivery][:datetime].in_time_zone('Pacific Time (US & Canada)')
   end
 
   def delivery_params
